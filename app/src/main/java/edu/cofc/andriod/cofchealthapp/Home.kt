1f27abc93bddd7f4d4a058f975613d0e6cbfc2a1
Zac <palmerzt@g.cofc.edu>
2023-04-08 21:01:59 -0400

    minor search and home changes

--- app/src/main/java/edu/cofc/andriod/cofchealthapp/Home.kt
+++ app/src/main/java/edu/cofc/andriod/cofchealthapp/Home.kt
@@ -10,21 +10,12 @@ import android.view.ViewGroup
 import edu.cofc.andriod.cofchealthapp.databinding.FragmentHomeBinding
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Home : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
-
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -41,7 +32,7 @@ class Home : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
