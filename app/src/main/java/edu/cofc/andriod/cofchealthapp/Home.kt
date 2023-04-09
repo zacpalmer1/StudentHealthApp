54ef7d28173308883c5604cc17180d9f4954e1b0
Zac <palmerzt@g.cofc.edu>
2023-04-08 20:45:34 -0400

    Phone call functionality implemented

--- app/src/main/java/edu/cofc/andriod/cofchealthapp/Home.kt
+++ app/src/main/java/edu/cofc/andriod/cofchealthapp/Home.kt
@@ -1,10 +1,14 @@
 package edu.cofc.andriod.cofchealthapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import edu.cofc.andriod.cofchealthapp.databinding.FragmentHomeBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,16 +21,21 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Home : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -34,8 +43,47 @@ class Home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.studentServices.setOnClickListener {
+            (activity as? MainActivity)?.apply {
+                output = "(843) 953-5668"
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:$output")
+                startActivity(intent)
+            }
+        }
+        binding.financialAid.setOnClickListener {
+            (activity as? MainActivity)?.apply {
+                output = "(843) 953-5540"
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:$output")
+                startActivity(intent)
+            }
+        }
+        binding.studentAffairs.setOnClickListener {
+            (activity as? MainActivity)?.apply {
+                output = "(843) 953-5522"
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:$output")
+                startActivity(intent)
+            }
+        }
+        binding.careerCenter.setOnClickListener {
+            (activity as? MainActivity)?.apply {
+                output = "(843) 953-5692"
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:$output")
+                startActivity(intent)
+            }
+        }
+        }
+
 
     companion object {
         /**
--- app/src/main/java/edu/cofc/andriod/cofchealthapp/MainActivity.kt
+++ app/src/main/java/edu/cofc/andriod/cofchealthapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package edu.cofc.andriod.cofchealthapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import edu.cofc.andriod.cofchealthapp.databinding.ActivityMainBinding
+import edu.cofc.andriod.cofchealthapp.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
 
+    var output = " "
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
