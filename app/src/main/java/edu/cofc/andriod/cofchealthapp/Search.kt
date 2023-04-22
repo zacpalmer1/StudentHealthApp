739d9eef53368fbfd2bc9abd15d10a4d79c0ec01
Zac Palmer <palmerzt@g.cofc.edu>
2023-04-22 00:00:34 -0400

    minor changes to post format

--- app/src/main/java/edu/cofc/andriod/cofchealthapp/Search.kt
+++ app/src/main/java/edu/cofc/andriod/cofchealthapp/Search.kt
@@ -1,6 +1,5 @@
 package edu.cofc.andriod.cofchealthapp
 
-
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -14,19 +13,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import edu.cofc.andriod.cofchealthapp.databinding.FragmentSearchBinding
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Search.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Search : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var binding: FragmentSearchBinding
@@ -45,12 +35,10 @@ class Search : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
     }
-    // Class for sorting data
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
