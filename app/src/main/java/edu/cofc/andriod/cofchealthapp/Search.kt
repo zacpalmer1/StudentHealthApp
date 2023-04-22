977a72fd58dc80580cbe81cad453fe886406fda4
Zac Palmer <palmerzt@g.cofc.edu>
2023-04-21 23:21:23 -0400

    Search functionality complete

--- app/src/main/java/edu/cofc/andriod/cofchealthapp/Search.kt
+++ app/src/main/java/edu/cofc/andriod/cofchealthapp/Search.kt
@@ -1,10 +1,16 @@
 package edu.cofc.andriod.cofchealthapp
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import edu.cofc.andriod.cofchealthapp.databinding.FragmentSearchBinding
 
@@ -24,6 +30,10 @@ class Search : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var binding: FragmentSearchBinding
+    lateinit var programmingLanguagesLV: ListView
+    lateinit var listAdapter: ArrayAdapter<String>
+    lateinit var programmingLanguagesList: ArrayList<String>;
+    lateinit var searchView: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -37,11 +47,111 @@ class Search : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
-
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
     // Class for sorting data
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        programmingLanguagesLV = binding.idLVProgrammingLanguages
+        searchView = binding.idSV
+        programmingLanguagesList = ArrayList()
+        programmingLanguagesList.add("Student Health Services")
+        programmingLanguagesList.add("Financial Aid Office")
+        programmingLanguagesList.add("Student Affairs")
+        programmingLanguagesList.add("Career Center")
+        programmingLanguagesList.add("Mental Stability")
+        programmingLanguagesList.add("Time Management")
+        programmingLanguagesList.add("Managing Stress")
+        programmingLanguagesList.add("Friendship Guide")
+
+        listAdapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            programmingLanguagesList
+        )
+
+        programmingLanguagesLV.adapter = listAdapter
+
+        programmingLanguagesLV.setOnItemClickListener { adapterView, view, position, id ->
+            when (val selectedItem = adapterView.getItemAtPosition(position) as String) {
+                "Student Health Services" -> {
+                    (activity as? MainActivity)?.apply {
+                        output = "(843) 953-5668"
+                        val intent = Intent(Intent.ACTION_DIAL)
+                        intent.data = Uri.parse("tel:$output")
+                        startActivity(intent)
+                    }
+                }
+                "Financial Aid Office" -> {
+                    (activity as? MainActivity)?.apply {
+                        output = "(843) 953-5540"
+                        val intent = Intent(Intent.ACTION_DIAL)
+                        intent.data = Uri.parse("tel:$output")
+                        startActivity(intent)
+                    }
+                }
+                "Student Affairs" -> {
+                    (activity as? MainActivity)?.apply {
+                        output = "(843) 953-5522"
+                        val intent = Intent(Intent.ACTION_DIAL)
+                        intent.data = Uri.parse("tel:$output")
+                        startActivity(intent)
+                    }
+                }
+                "Career Center" -> {
+                    (activity as? MainActivity)?.apply {
+                        output = "(843) 953-5692"
+                        val intent = Intent(Intent.ACTION_DIAL)
+                        intent.data = Uri.parse("tel:$output")
+                        startActivity(intent)
+                    }
+                }
+                "Mental Stability" -> {
+                    val openURL = Intent(Intent.ACTION_VIEW)
+                    openURL.data = Uri.parse("https://www.helpguide.org/articles/mental-health/building-better-mental-health.htm")
+                    startActivity(openURL)
+                }
+                "Time Management" -> {
+                    val openURL = Intent(Intent.ACTION_VIEW)
+                    openURL.data = Uri.parse("https://hbr.org/2020/01/time-management-is-about-more-than-life-hacks")
+                    startActivity(openURL)
+                }
+                "Managing Stress" -> {
+                    val openURL = Intent(Intent.ACTION_VIEW)
+                    openURL.data = Uri.parse("https://www.webmd.com/balance/stress-management/stress-management")
+                    startActivity(openURL)
+                }
+                "Friendship Guide" -> {
+                    val openURL = Intent(Intent.ACTION_VIEW)
+                    openURL.data = Uri.parse("https://www.sonderwellness.com/blog/2022/08/22/healthy-friendships/")
+                    startActivity(openURL)
+                }
+
+                else -> {
+                    Toast.makeText(requireContext(), "Clicked $selectedItem", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (programmingLanguagesList.contains(query)) {
+
+                    listAdapter.filter.filter(query)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                listAdapter.filter.filter(newText)
+                return false
+            }
+
+        })
+
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
