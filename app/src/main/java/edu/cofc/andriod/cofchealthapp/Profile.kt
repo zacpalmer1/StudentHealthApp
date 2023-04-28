b01c917dd9327fd6132dcaf0d0b7ec0b1226288c
Zac Palmer <palmerzt@g.cofc.edu>
2023-04-27 21:43:12 -0400

    Successfully pull from Database

--- app/src/main/java/edu/cofc/andriod/cofchealthapp/Profile.kt
+++ app/src/main/java/edu/cofc/andriod/cofchealthapp/Profile.kt
@@ -1,10 +1,21 @@
 package edu.cofc.andriod.cofchealthapp
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import edu.cofc.andriod.cofchealthapp.databinding.FragmentProfileBinding
+import org.checkerframework.checker.nullness.qual.NonNull
 
 
 private const val ARG_PARAM1 = "param1"
@@ -13,7 +24,7 @@ private const val ARG_PARAM2 = "param2"
 class Profile : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
-
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -26,8 +37,28 @@ class Profile : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FirebaseApp.initializeApp(requireContext())
+        val db = Firebase.firestore
+        // Get a reference to the collection
+        val docRef = db.collection("usersScore").document("5ccOvKbEKGngZr1OKo0s")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    val total = document.getString("score") // get the value of the 'population' field
+                    binding.dailyScore.text = "$total" // display the population in a TextView using data binding
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
     }
 
     companion object {
